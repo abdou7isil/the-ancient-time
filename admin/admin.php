030d30ca02898606bf3c2f39a9c3170c1e0331ab
mederbel <mederbelsofiane@gmail.com>
2020-01-08 20:58:43 +0100

    organisation de la structure du projet

---
+++ admin/admin.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+<link rel="stylesheet" type="text/css" href="../css/default.css" media="screen"/>
+<title>The Ancient Time</title>
+
+</head>
+<body>
+
+<div id="Layer1" ><img src="../img/logo.png" alt="" width="232" height="411" /></div>
+<div class="header" id="Layer3">
+  <pre><a href="#" class="style8 style12">
+
+Create an account</a>
+
+<label><span class="style11">Login<br /></span></label><input name="login" type="text" placeholder= "your user name" /><label><span class="style11"><br />Password<br /><input name="login" type="password" placeholder="***********"  /></span></label>
+  </pre>
+  <form id="form1" method="post" action="">
+    <pre class="style13"><input type="submit" name="Submit" value="Valider" />
+    </pre>
+  </form>
+
+  <pre><a href="#" class="style8 style12">Forgot your password ?</a>
+
+</pre>
+</div>
+
+<div id="Layer7"><img src="../img/45545444.png" alt="" width="132" height="89" /></div>
+<div class="container">
+   <div id="Layer5">
+  <h1 align="center" class="style4">&nbsp;</h1>
+  <h1 align="center" class="style6">The Ancient Time </h1>
+</div>
+   <div class="gfx"></div>
+  <div class="top">
+    <div class="navigation"> <a href="#" id="selected">home</a> <a href="#">forum</a> <a href="#">property</a> <a href="#">player</a> <a href="#">history</a> </div>
+    <div class="pattern"><span></span></div>
+    <div class="header">
+
+      <h1 align="right">  The Ancient Time </h1>
+      <p  align="center" class="style2"> the greatest war and treasure hunt </p>
+    </div>
+    <div class="pattern"><span></span></div>
+  </div>
+  <div>
+  <p>       </p>
+  </div>
+  <div class="content">
+    <div class="welcome">
+      <p> </p>
+	<p align="center">&nbsp;</p>
+	<p align="center" ><b>Here you are on the land of the ancient time ...</b></p>
+	 <p align="center"> Where the biggest historical epic in order to find hidden weapons and treasures, will you be able to find as much of the five thrones as possible? </p>
+	 <p align="center"> Can you bypass monsters and all obstacles? </p>
+	 <p align="center">Will you be the winner and win the supremacy of the Five Thrones?</p>
+	  <p align="center"><b>Come find the answers to these questions yourself.</b></p>
+	 <p align="center"> But beware: Today, in the land of antiquity, 1 humain, 68 monsters and 3 demons have died in combat ...</p>
+	</div>
+    <div class="divider">
+      <p>&nbsp;</p>
+    </div>
+  </div>
+   <div class="footer">
+     <p>&copy;</p>
+  </div>
+</div>
+</body>
+</html>
