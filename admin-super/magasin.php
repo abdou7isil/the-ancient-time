d35ef9ce2f2edfd3a891002e8dff2c80c83bbc72
mounirProgrammer <57832867+mounirProgrammer@users.noreply.github.com>
2020-02-10 08:21:18 +0100

    Update magasin.php

--- admin-super/magasin.php
+++ admin-super/magasin.php
@@ -1,20 +1,6 @@
 
 <!DOCTYPE html>
-<?php
-$sql_name="root";
-$sql_pass="";
-$server_name="localhost";
-$db_name="oo";
-$conn = new mysqli($server_name, $sql_name, $sql_pass, $db_name);
-// Check connection
-if ($conn->connect_error) {
-  echo "string";
-    die("Connection failed: " . $conn->connect_error);
-}
 
-
-
-?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -36,16 +22,13 @@ if ($conn->connect_error) {
         position: static;
       }
     }
+
     </style>
-<style>
-.buttom:hover {background-color: #ffb84d;}
 
-
-</style>
   </head>
   <body>
     <?php
-   include'../html/admin/navbar.php'
+   include'../html/comptable/navbar.php'
      ?>
       <section>
         <div class="container-fluid ">
@@ -65,17 +48,12 @@ if ($conn->connect_error) {
                     </th>
                   </thead>
                   <tbody>
-                    <?php
-                    $query="select quantité from revenus";
-                    $result=$conn->query($query);
-                    while($row = $result->fetch_assoc()) {
-                    echo ' <tr>
+                    <tr>
                       <td style="border-width: medium;background-color: #c2c2a3;" ><strong>quantité</strong></td>
-                      <td style="border-width: medium;">'; echo $row;echo '</td>
-                      <td style="border-width: medium;">'; echo $row; echo '</td>
-                      <td style="border-width: medium;">'; echo $row; echo '</td>
-                    </tr>';}
-                    ?>
+                      <td style="border-width: medium;">1</td>
+                      <td style="border-width: medium;">2</td>
+                      <td style="border-width: medium;">3</td>
+                    </tr>
                   </tbody>
                 </table><br>
 
@@ -101,8 +79,6 @@ if ($conn->connect_error) {
                     </tr>
                   </tbody>
                 </table>
-                <br>
-
                 <form>
                   <tr>
                     <td style="border : none"><h1>  </h1> </td>
@@ -117,82 +93,9 @@ if ($conn->connect_error) {
                     <td style="border : none"><input type="textField" required></td>
                     <td style="border : none"><strong>pourcentage (%)   </strong></td>
                     <td style="border : none"><input type="textField" required></td>
-                    <td style="border : none"><input class="buttom" type="submit" value="valider" style="color:#006600"></td>
-                  </tr>
-                </form><br>
-
-                <table padding="10px" style="border-width: medium;">
-                  <h2>Les objet</h2>
-                  <th style="background-color: #c2c2a3;">
-                    <td style="border-width: medium; background-color: #c2c2a3;" ><strong>ID objet</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>Nom d'object</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>le prix</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>Etat d'objet</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>La force</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>Point d'action</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>Point de mouvment</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>Point de vie</strong></td>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>La résistance</strong></td>
-                  </th>
-                  <tr>
-                    <td style="border-width: medium; background-color: #c2c2a3;"><strong>Objet</strong></td>
-                    <td style="border-width: medium;">1</td>
-                    <td style="border-width: medium;">épée</td>
-                    <td style="border-width: medium;">10</td>
-                    <td style="border-width: medium;">neu</td>
-                    <td style="border-width: medium;">70</td>
-                    <td style="border-width: medium;">0</td>
-                    <td style="border-width: medium;">0</td>
-                    <td style="border-width: medium;">80</td>
-                    <td style="border-width: medium;">15</td>
-                  </tr>
-                </table><br>
-                <div class="ajouter">
-                <h2>Ajouter un objet</h2>
-                <form>
-                  <table style="border :none">
-                  <tr><td style="border :none"><label>ID objet</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>Nom objet</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>Etat objet</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>L prix objet</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>La force</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>Point d'action</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>Point de mouvment</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>Point de vie</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                  </tr><tr><td style="border :none"><label>Résistance</label></td>
-                  <td style="border :none"><input type="textField" required></td>
-                </tr><tr><td style="border :none"><input class="buttom"type="submit" value="ajouter" ;  style="width:150px ; height:35px; color:#006600"><br></td></tr>
-
-
-                </form>
-              </div>
-                <div class="supprimer">
-                <form>
-                  <tr>
-                    <td style="border : none"><h1>  </h1> </td>
-                  </tr>
-
-                  <tr>
-                    <td style="border : none">  <h2>Supprimer objet</h2></td>
-                  </tr>
-                  <tr>
-
-                    <td style="border : none"><strong><h2>ID</h2>  </strong></td>
-                    <td style="border : none"><input type="textField" required></td>
-                    <td style="border : none"><input class="buttom" type="submit" value="supprimer" style="color:#e60000"></td>
+                    <td style="border : none"><input type="submit" value="valider"></td>
                   </tr>
                 </form>
-              </div>
-
-
 
 
 
@@ -202,7 +105,6 @@ if ($conn->connect_error) {
       </div>
     </div>
       </section>
-
 
 
 
